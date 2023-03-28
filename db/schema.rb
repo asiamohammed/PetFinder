e1ee6ac773b049adb0f5f3e651887afa8e12cc63
asiamohammed <mohamedasia021@gmail.com>
2023-03-28 13:47:59 +0300

    user model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_28_094519) do
+ActiveRecord::Schema.define(version: 2023_03_28_104645) do
 
   create_table "pets", force: :cascade do |t|
     t.string "image"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2023_03_28_094519) do
     t.string "animal_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
   end
 
 end
